9aebc6971d56ddd16eff391391774af86f1d306b
wallet_wr <yokond0013@gmail.com>
2017-10-13 21:33:53 +0900

    first commit

---
+++ index.js
@@ -0,0 +1,27 @@
+const Syslogd = require('syslogd')
+
+const co            = require("co");
+const moment        = require('moment');
+
+const port = process.argv[2];
+const mongo_host = process.argv[3];
+const MongoClient = require("mongodb").MongoClient;
+const url = "mongodb://" + mongo_host + ":27017/procyon";
+
+
+Syslogd(function(info) {
+    co(function* () {
+        console.log("info" , info);
+
+        db = yield MongoClient.connect(url);
+        yield db.collection("syslog").insertOne(info);
+        yield db.close();
+    }).catch(function(err){
+        process.on('unhandledRejection', console.log(err));
+    });
+}).listen(port, function(err) {
+    console.log('listen :' + port)
+
+    if(err)
+        console.log(err);
+})
